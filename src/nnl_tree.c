9c68cfb4b22d8c0ad37cc99d65eb670b760f60b1
dscp46 <celestine+dev@gramaize.eu>
2026-07-30 22:34:50 +0200

    Add NNL utility functions, pseudo code to verify...

--- src/nnl_tree.c
+++ src/nnl_tree.c
@@ -1,4 +1,10 @@
+#define NNL_TREE_DEF
 #include "nnl_tree.h"
+
+#include <utstack.h>
+
+// Tree root address
+const nnl_addr_t nnl_root = 0x80000000;
 
 uint32_t nnl_tree_size( uint8_t scheme_depth)
 {
@@ -71,4 +77,106 @@ int nnl_decode_uv( const nnl_sd_t *sd, nnl_addr_t *u, nnl_addr_t *v)
 	return 1;
 }
 
+uint8_t nnl_depth( nnl_addr_t u)
+{
+	// assert( u );
+	uint8_t depth = 32;
+	uint32_t mask = 0xFFFFFFFF;
+	while(!( u & ~mask ))
+	{
+		mask <<= 1;
+		--depth;
+	}
+	return depth;
+}
 
+uint32_t nnl_nb_leaves( nnl_addr_t u, uint8_t scheme_depth)
+{
+	// assert( scheme_depth < 8*sizeof(nnl_addr_t) );
+	return 1 << ( scheme_depth - nnl_depth(u) );
+}
+
+nnl_state_t nnl_node_state( nnl_addr_t u, uint8_t scheme_depth, uint32_t *rvk_tree)
+{
+	uint32_t offset = nnl_tree_offset( u);
+	if( !rvk_tree )
+		return NNL_ST_REVOKED; // FIXME
+	// assert( offset < (sizeof( rvk_tree ) / sizeof(uint32_t))) ?
+
+	if( rvk_tree[offset] == 0 )
+		return NNL_ST_VALID;
+
+	if( rvk_tree[offset] == nnl_nb_leaves( u, scheme_depth) )
+		return NNL_ST_REVOKED;
+
+	return NNL_ST_MIXED;
+}
+
+/** [!PSEUDO-CODE]
+cover_tree()
+{
+	nnl_addr_t cur, i, left_child, right_child, *stack = NULL;
+	nnl_state_t cur_state, left_state, right_state;
+
+	UT_icd ud_sd_ent_icd;
+	UT_array *sd_tree;
+	utarray_new( sd_tree, &ut_sd_end_icd);
+
+	STACK_PUSH( stack, nnl_root);
+
+	while(!STACK_EMPTY( stack))
+	{
+		STACK_POP( stack, cur);
+		cur_state = nnl_node_state( cur, scheme_depth, rvk_tree);
+
+		if( cur_state == NNL_ST_VALID )
+		{
+			emit( T[cur] ); // aes_g3( G_DIR_PROCESS, node_key( cur), sd->key ); utarray_push_back( sd_tree, sd);
+			continue;
+		}
+
+		if( cur_state == NNL_ST_REVOKED )
+			continue;
+
+		// cur is MIXED. Descend until the revoked region branches
+		i = cur;
+		while(true)
+		{
+			left_child = nnl_left( i);
+			left_state = nnl_node_state( left_child, scheme_depth, rvk_tree);
+
+			right_child = nnl_right( i);
+			right_state = nnl_node_state( right_child, scheme_depth, rvk_tree);
+
+			if( left_state == NNL_ST_REVOKED && right_state == NNL_ST_REVOKED)
+				break; // Safeguard
+
+			if( left_state == NNL_ST_REVOKED )
+			{
+				emit_diff( i, left_child); // S{i,L} = T_i \ T_L
+				// ??? aes_g3( G_DIR_LEFT, node_key( i), sd->key ); utarray_push_back( sd_tree, sd);
+				i = right_child;
+			}
+			else if( right_state == NNL_ST_REVOKED )
+			{
+				emit_diff( i, right_child); // S{i,R} = T_i \ T_R
+				// ??? aes_g3( G_DIR_RIGHT, node_key( i), sd->key ); utarray_push_back( sd_tree, sd);
+				i = left_child;
+			}
+			else
+			{
+				// Both children are MIXED
+				STACK_PUSH( stack, left_child);
+				STACK_PUSH( stack, right_child);
+				break;
+			}
+
+			if( nnl_node_state( i, scheme_depth, rvk_tree) == NNL_ST_VALID )
+			{
+				emit( T[i] ); // aes_g3( G_DIR_PROCESS, node_key( i), sd->key ); utarray_push_back( sd_tree, sd);
+				break;
+			}
+		}
+	}
+}
+ **/
--- src/nnl_tree.h
+++ src/nnl_tree.h
@@ -6,6 +6,11 @@
 // Node address (path to the leaf, followed by a terminal '1' bit).
 typedef uint32_t nnl_addr_t;
 
+#ifndef NNL_TREE_DEF
+// Tree root address
+extern const nnl_addr_t nnl_root;
+#endif	/* NNL_TREE_DEF */
+
 // Subset-Difference UV structure
 typedef struct nnl_sd nnl_sd_t;
 struct nnl_sd {
@@ -13,9 +18,19 @@ struct nnl_sd {
 	nnl_addr_t uv;
 };
 
+typedef enum {
+	NNL_ST_VALID,
+	NNL_ST_MIXED,
+	NNL_ST_REVOKED
+} nnl_state_t;
+
 uint32_t nnl_tree_size( uint8_t scheme_depth);
 uint32_t nnl_tree_offset( nnl_addr_t node_addr);
 int nnl_encode_uv( nnl_addr_t u, nnl_addr_t v, nnl_sd_t *sd);
 int nnl_decode_uv( const nnl_sd_t *sd, nnl_addr_t *u, nnl_addr_t *v);
+
+uint8_t nnl_depth( nnl_addr_t u);
+uint32_t nnl_nb_leaves( nnl_addr_t u, uint8_t scheme_depth);
+nnl_state_t nnl_node_state( nnl_addr_t u, uint8_t scheme_depth, uint32_t *rvk_tree);
 
 #endif	/* NNL_TREE_H */
